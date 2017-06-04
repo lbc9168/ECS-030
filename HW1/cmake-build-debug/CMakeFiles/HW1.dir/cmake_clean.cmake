92e31f2c8e32e89dbb33c7179c373e94c14160d6
Bingcai Liu <liu.3444.osu@gmail.com>
2017-06-03 18:09:58 -0700

    Changes

---
+++ HW1/cmake-build-debug/CMakeFiles/HW1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW1.dir/main.c.o"
+  "CMakeFiles/HW1.dir/change.cpp.o"
+  "HW1.pdb"
+  "HW1.exe"
+  "libHW1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/HW1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
